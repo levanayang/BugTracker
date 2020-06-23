116a9717ad2dbc48f160c4be0a440d475f0215ac
levanayang <levanavyang@gmail.com>
2020-06-23 10:13:10 -0700

    added model for bug tickets and ticket database context class

---
+++ Data/TicketContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BugTracker.Models;
+
+namespace BugTracker.Data
+{
+    public class TicketContext : DbContext
+    {
+        public TicketContext(DbContextOptions<TicketContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ticket> Ticket { get; set; }
+    }
+}
---
+++ Models/Ticket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Models
+{
+    public enum Status { Open, Resolved, InProgress }
+    public class Ticket
+    {
+        // Main Display info
+        public int Id { get; set; }
+        public string Priority { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Status")]
+        public Status TicketStatus { get; set; }
+
+        // Date info
+        [Display(Name = "Opened")]
+        [DataType(DataType.Date)]
+        public DateTime OpenDate { get; set; }
+        [Display(Name = "Due")]
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+        [Display(Name = "Closed")]
+        [DataType(DataType.Date)]
+        public DateTime CloseDate { get; set; }
+
+        // Details
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
